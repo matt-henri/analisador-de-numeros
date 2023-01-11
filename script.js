5c7f6c8b1795923236d4ec8eae3167a941307497
Mateus Henrique <mathmarkub@gmail.com>
2023-01-11 19:50:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -24,8 +24,26 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista!')
+    }
+    num.value = ''
+    num.Focus()
+} 
+
+
+function finalizar() {
+    if(valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    }   else {
+        let tot = valores.length
+
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo, temos ${tot} números cadastrados.<p>`
     }
 }
